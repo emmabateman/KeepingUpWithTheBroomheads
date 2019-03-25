ec80f250bd4b885ac9b0b8fb568a525190eaa35d
emmabateman <emmalbateman@gmail.com>
2019-03-25 13:42:59 -0700

    restored working data structure

--- KeepingUpWithTheBroomheads/src/keepingupwiththebroomheads/KeepingUpWithTheBroomheads.java
+++ KeepingUpWithTheBroomheads/src/keepingupwiththebroomheads/KeepingUpWithTheBroomheads.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,11 +27,11 @@ public class KeepingUpWithTheBroomheads {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        java.awt.EventQueue.invokeLater(() -> {
+/*        java.awt.EventQueue.invokeLater(() -> {
             BroomheadJFrame frame = new BroomheadJFrame();
             frame.setVisible(true);
         });
-        
+*/        
         Family = new LinkedList<Person>();
         loadFromCSV("family.csv");
         
--- KeepingUpWithTheBroomheads/src/keepingupwiththebroomheads/Person.java
+++ KeepingUpWithTheBroomheads/src/keepingupwiththebroomheads/Person.java
@@ -9,59 +9,86 @@ import java.util.LinkedList;
 /**
  *
  * @author emma
- *
+ * 
  * represent a person and the connections that tie them to the family tree
  */
-public abstract class Person {
+public class Person {
     private String name;
+    
+    private LinkedList<Person> parents;
     private LinkedList<Person> children;
-
+    private LinkedList<Person> partners;
+    
     /**
      * constructor
      * @param name name of the person
      */
     public Person(String name) {
         this.name = name;
+        this.parents = new LinkedList<Person>();
         this.children = new LinkedList<Person>();
+        this.partners = new LinkedList<Person>();
     }
-    /**
-     * Returns an array containing the parents of the given person. If the person is an
-     * indirectRelative, their parents are irrelevent and null is returned.
-     */
-    abstract Person[] getParents();
-
-    /**
-     * Returns an array containing every relevant partner the person has had.
-     * If they have no partners, returns an empty array
-     */
-    abstract Person[] getPartners();
-
-    /**
-     * Returns an array containing this person's children.
-     * If they have no children, an empty array is returned.
-     */
-    public Person[] getChildren() {
-      Person[] childArray = new Person[children.size()];
-      for(int i = 0; i < children.size(); i++) {
-        childArray[i] = children.get(i);
-      }
-      return childArray;
-    }
+    
     /**
      * add a relationship to one of the lists
-     * @param relationType The relationship to the person
+     * @param relationType parent, child, or partner
      * @param person person object being added to list
      */
-    abstract void addRelation(String relationType, Person person);
-
-
-
-
+    public void addRelation(String relationType, Person person) {
+        if (relationType.equals("parent")) {
+            this.parents.add(person);
+        }
+        else if (relationType.equals("child")) {
+            this.children.add(person);
+        }
+        else if (relationType.equals("partner")) {
+            this.partners.add(person);
+        }
+    }
+    
     /**
      * @return name
      */
     public String getName() {
         return this.name;
     }
-
+    
+    /**
+     * @return list of parents
+     */
+    public LinkedList<Person> getParents() {
+        return this.parents;
+    }
+    
+    /**
+     * @return list of children
+     */
+    public LinkedList<Person> getChildren() {
+        return this.children;
+    }
+    
+    /**
+     * @return list of partners
+     */
+    public LinkedList<Person> getPartners() {
+        return this.partners;
+    }
+    
+    /**
+     * @return list of siblings
+     */
+    public LinkedList<Person> getSiblings() {
+        LinkedList<Person> siblings = new LinkedList<Person>();
+        for (int i = 0; i < this.parents.size(); i++) {
+            for (int j = 0; j < this.parents.get(i).children.size(); j++) {
+                Person parentsChild = this.parents.get(i).children.get(j);
+                if (!siblings.contains(parentsChild) && parentsChild != this) {
+                    siblings.add(parentsChild);
+                }
+            }
+        }
+        return siblings;
+    }
+    
 }
